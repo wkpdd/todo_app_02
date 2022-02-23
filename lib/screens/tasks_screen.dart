b9c3067ba427c0978d202856e3ba5e1382eca3a3
Elwannas HIRI <hiri.elwannas@gmail.com>
2022-02-23 18:11:26 +0100

    great improvement

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app_02/classes/task_class.dart';
+import 'package:todo_app_02/providers/task_provider.dart';
+import 'package:todo_app_02/widgets/add_note_bottom_sheet.dart';
+import 'package:todo_app_02/widgets/tasks_list.dart';
+import 'package:provider/provider.dart';
 
 class TasksScreen extends StatefulWidget {
   const TasksScreen({Key? key}) : super(key: key);
@@ -10,51 +15,69 @@ class TasksScreen extends StatefulWidget {
 class _TasksScreenState extends State<TasksScreen> {
   int numberOfTasks = 0;
 
+  List<Widget> _header() {
+    return [
+      Container(
+          margin: const EdgeInsets.only(top: 70, bottom: 20, left: 50),
+          width: 70,
+          height: 70,
+          decoration: BoxDecoration(
+              color: Colors.white, borderRadius: BorderRadius.circular(50)),
+          child: const Icon(
+            Icons.list,
+            color: Colors.lightBlue,
+            size: 40,
+          )),
+      Container(
+        margin: const EdgeInsets.only(bottom: 10, left: 50),
+        child: const Text(
+          "Khedmoey",
+          style: TextStyle(
+              fontFamily: "FiraCode",
+              fontSize: 30,
+              color: Colors.white,
+              fontWeight: FontWeight.bold),
+        ),
+      ),
+      Container(
+        margin: const EdgeInsets.only(bottom: 40, left: 50),
+        child: Text(
+          "${context.read<TaskProvider>().tasks.length} Sla7",
+          style: const TextStyle(
+              fontFamily: "FiraCode",
+              fontSize: 14,
+              color: Colors.white,
+              fontWeight: FontWeight.bold),
+        ),
+      )
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          showModalBottomSheet(
+              backgroundColor: Colors.transparent,
+              context: context,
+              builder: (_) => AddNoteBottomSheet(func: (TTask task) {
+                    setState(() {
+                      context.read<TaskProvider>().addNewTask(task);
+                    });
+                  }));
+        },
+      ),
       backgroundColor: Colors.lightBlue,
       body: SafeArea(
         child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Container(
-                  margin: const EdgeInsets.only(top: 70, bottom: 20, left: 50),
-                  width: 70,
-                  height: 70,
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(50)),
-                  child: const Icon(
-                    Icons.list,
-                    color: Colors.lightBlue,
-                    size: 40,
-                  )),
-              Container(
-                margin: const EdgeInsets.only(bottom: 10, left: 50),
-                child: const Text(
-                  "Khedmoey",
-                  style: TextStyle(
-                      fontFamily: "FiraCode",
-                      fontSize: 30,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold),
-                ),
-              ),
-              Container(
-                margin: const EdgeInsets.only(bottom: 40, left: 50),
-                child: Text(
-                  "$numberOfTasks Sla7",
-                  style: const TextStyle(
-                      fontFamily: "FiraCode",
-                      fontSize: 14,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold),
-                ),
-              ),
+              ..._header(),
               Expanded(
                 child: Container(
                     width: size.width,
@@ -65,35 +88,9 @@ class _TasksScreenState extends State<TasksScreen> {
                         borderRadius: BorderRadius.only(
                             topLeft: Radius.circular(10),
                             topRight: Radius.circular(10))),
-                    child: TasksList()),
+                    child: TasksList(context.read<TaskProvider>().tasks)),
               )
             ]),
-      ),
-    );
-  }
-}
-
-class TasksList extends StatelessWidget {
-  const TasksList({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView(
-      children: List.generate(50, (index) => TaskTile()),
-    );
-  }
-}
-
-class TaskTile extends StatelessWidget {
-  const TaskTile({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      title: Text("Test"),
-      trailing: Checkbox(
-        value: false,
-        onChanged: ((value) {}),
       ),
     );
   }
